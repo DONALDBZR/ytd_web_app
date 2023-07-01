61cdab3081c9e1d6147171f3e756bfdb75c78fff
DONALDBZR <andygaspard@hotmail.com>
2023-07-01 14:14:55 +0400

    [UPDATE 0.0.8] Documenting the application

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+# Importing the requirements for the application
+from flask import Flask, render_template, url_for
+# Instantiating the application
 Application = Flask(__name__)
 
 

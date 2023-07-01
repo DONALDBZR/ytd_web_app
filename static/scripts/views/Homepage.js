67852c2d8cd840aca0782f84a76fa0af5910e67a
DONALDBZR <andygaspard@hotmail.com>
2023-07-01 15:20:30 +0400

    [UPDATE 0.0.12] Adding the homepage view of the application

---
+++ static/scripts/views/Homepage.js
@@ -0,0 +1,116 @@
+/**
+ * The Application that is going to be rendered in the DOM
+ */
+class Application extends React.Component {
+    /**
+     * Constructing the application from React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            System: {
+                color_scheme: "light",
+            },
+        };
+    }
+    /**
+     * Rendering the application by instantiating its components
+     * @returns {HTMLBodyElement}
+     */
+    render() {
+        return [<Header />, <Main />, <Footer />];
+    }
+}
+/**
+ * The component that is the header
+ */
+class Header extends Application {
+    /**
+     * Constructing the header component and also inheriting the properties and states from the application
+     * @param {*} props
+     */
+    constructor(props) {
+        super(props);
+    }
+    /**
+     * Rendering the component
+     * @returns {HTMLHeaderElement}
+     */
+    render() {
+        return (
+            <header>
+                <nav>
+                    <div class="active">
+                        <a href="/">YouTube Downloader</a>
+                    </div>
+                    <div>
+                        <a href="/search">
+                            <i class="fa fa-search"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <ColorScheme />
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+}
+/**
+ * The color scheme
+ */
+class ColorScheme extends Header {
+    /**
+     * Constructing the color scheme's component and also inheriting the properties and states from the header
+     * @param {*} props
+     */
+    constructor(props) {
+        super(props);
+    }
+    /**
+     * Rendering the component which allows the user to change the color scheme
+     * @returns {HTMLButtonElement}
+     */
+    render() {
+        if (this.state.System.color_scheme == "light") {
+            return (
+                <button>
+                    <i class="fa-solid fa-toggle-off"></i>
+                </button>
+            );
+        } else {
+            return (
+                <button>
+                    <i class="fa-solid fa-toggle-on"></i>
+                </button>
+            );
+        }
+    }
+}
+/**
+ * The component that is the main
+ */
+class Main extends Application {
+    /**
+     * Rendering the component
+     * @returns {HTMLMainElement}
+     */
+    render() {
+        return <main></main>;
+    }
+}
+/**
+ * The component that is the footer
+ */
+class Footer extends Application {
+    /**
+     * Rendering the component
+     * @returns {HTMLFooterElement}
+     */
+    render() {
+        return <footer></footer>;
+    }
+}
+// Rendering the page
+ReactDOM.render(<Application />, document.body);

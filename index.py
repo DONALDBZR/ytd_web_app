505a57e55b1dbee29326a423d66569d2c051abb1
DONALDBZR <andygaspard@hotmail.com>
2023-07-04 18:16:14 +0400

    [UPDATE 0.0.30] Documenting the code

--- index.py
+++ index.py
@@ -1,9 +1,18 @@
 # Importing the requirements for the application
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, request
+from flask_session import Session
+from datetime import datetime
 # Instantiating the application
 Application = Flask(__name__)
+# Configuring the application for using sessions
+Application.config["SESSION_TYPE"] = 'filesystem'
+Session(Application)
 
 
 @Application.route('/')
 def homepage():
+    # Rendering the template needed which will import the web-worker
+
+    # Returns:
+    #   (str): The template which is stringified version of a HTML file
     return render_template('page.html')

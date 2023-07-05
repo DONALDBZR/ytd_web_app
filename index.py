d34e0697c46c3d5133dcc06f33302fbe4d1a9954
DONALDBZR <andygaspard@hotmail.com>
2023-07-05 10:15:58 +0400

    [UPDATE 0.0.36] Restructuring the codes

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 # Importing the requirements for the application
 from flask import Flask, render_template, url_for
+from flask_session import Session
 from Session import SessionManager
 # Instantiating the application
 Application = Flask(__name__)
 # Configuring the application for using sessions
 Application.config["SESSION_TYPE"] = 'filesystem'
+Session(Application)
 
 
 @Application.route('/')

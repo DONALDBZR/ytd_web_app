2a9ba8ca4f8794ca687820292b44c1753cb6387e
DONALDBZR <andygaspard@hotmail.com>
2023-07-05 10:30:17 +0400

    [UPDATE 0.0.38] Sending the session data in the form of JSON

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 # Importing the requirements for the application
-from flask import Flask, render_template, url_for
+from flask import Flask, Response, render_template, url_for, jsonify
 from flask_session import Session
 from Session import SessionManager
 # Instantiating the application
 Application = Flask(__name__)
+Session_Manager = SessionManager()
 # Configuring the application for using sessions
 Application.config["SESSION_TYPE"] = 'filesystem'
 Session(Application)
@@ -17,3 +18,13 @@ def homepage() -> str:
     Returns: (str): The template which is stringified version of a HTML file
     """
     return render_template('page.html')
+
+
+@Application.route('/Session')
+def getSession() -> Response:
+    """
+    Sending the session data in the form of JSON
+
+    Returns: (Response): JSON containing the session data
+    """
+    return jsonify(Session_Manager.getSession())

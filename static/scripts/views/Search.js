eb530806a0e888d56a24701a2552f52a4d367999
DONALDBZR <andygaspard@hotmail.com>
2023-07-22 16:56:07 +0400

    [UPDATE 0.1.06] Search::Main preparation

--- static/scripts/views/Search.js
+++ static/scripts/views/Search.js
@@ -202,17 +202,7 @@ class Main extends Application {
     render() {
         return (
             <main>
-                <p>
-                    The aim of the application is that software and contents
-                    must be free and it allows anyone to get content from
-                    certain platforms to be obtained for free as it is an
-                    application developed for people by people.
-                </p>
-                <div>
-                    <div>
-                        <i class="fa-brands fa-youtube"></i>
-                    </div>
-                </div>
+                <div>Main</div>
             </main>
         );
     }

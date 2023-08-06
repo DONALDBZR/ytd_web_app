6ff1f5c1404c005ff5779c85697feebfa0222d1e
DONALDBZR <andygaspard@hotmail.com>
2023-08-06 19:11:41 +0400

    [UPDATE 0.1.170] Constructing the application from React's Component

--- static/scripts/views/Header.js
+++ static/scripts/views/Header.js
@@ -1,6 +1,19 @@
 /**
  * The component that is the header for all of the pages
  */
-class Header extends React.Component {}
+class Header extends React.Component {
+    /**
+     * Constructing the application from React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            System: {
+                color_scheme: "",
+            },
+        };
+    }
+}
 // Rendering the page
 ReactDOM.render(<Application />, document.querySelector("header"));

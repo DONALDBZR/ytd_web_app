4245dbf0ba7b141f93931a9cc9629ad8b3506304
DONALDBZR <andygaspard@hotmail.com>
2023-08-20 17:39:46 +0400

    UPDATE [0.3.04] Retrieving data from the database.

--- ObjectRelationalMapper.py
+++ ObjectRelationalMapper.py
@@ -11,3 +11,15 @@ class Object_Relational_Mapper(Database_Handler):
         Connecting the database as well as initializing the ORM.
         """
         super().__init__()
+
+    def get_table_records(self) -> list:
+        """
+        Retrieving data from the database.  (SELECT)
+
+        Returns:
+            array: The list of data retrieved.
+        """
+        query = "SELECT * FROM table JOIN table1 ON table.id = table1.id WHERE a = b ORDER BY a ASC"
+        super().query(query, parameters)
+        super().execute()
+        return super().resultSet()

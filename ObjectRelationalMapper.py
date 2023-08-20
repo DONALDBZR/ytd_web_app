dac5f251c484e5d87ec769945d469117bc816f35
DONALDBZR <andygaspard@hotmail.com>
2023-08-20 17:42:06 +0400

    UPDATE [0.3.05] The query to be used to be sent to the database server to either get, post, update or delete data.

--- ObjectRelationalMapper.py
+++ ObjectRelationalMapper.py
@@ -5,12 +5,25 @@ class Object_Relational_Mapper(Database_Handler):
     """
     It is the relational mapper that will be used to simplify the process to entering queries.
     """
+    __query: str
+    """
+    The query to be used to be sent to the database server to either get, post, update or delete data.
+
+    Type: string
+    Visibility: private
+    """
 
     def __init__(self):
         """
         Connecting the database as well as initializing the ORM.
         """
         super().__init__()
+
+    def get_query(self) -> str:
+        return self.__query
+
+    def set_query(self, query: str) -> None:
+        self.__query = query
 
     def get_table_records(self) -> list:
         """

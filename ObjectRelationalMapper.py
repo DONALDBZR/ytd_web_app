4c0e7be823637bf5e81759b1be7283756039a99a
DONALDBZR <andygaspard@hotmail.com>
2023-08-21 21:25:45 +0400

    UPDATE [0.3.14] Parameters that the will be used to sanitize the query which is either  get, post, update or delete.

--- ObjectRelationalMapper.py
+++ ObjectRelationalMapper.py
@@ -14,6 +14,14 @@ class Object_Relational_Mapper(Database_Handler):
     Type: string
     Visibility: private
     """
+    __parameters: tuple | None
+    """
+    Parameters that the will be used to sanitize the query which
+    is either  get, post, update or delete.
+
+    Type: array|null
+    Visibility: private
+    """
 
     def __init__(self):
         """
@@ -27,18 +35,24 @@ class Object_Relational_Mapper(Database_Handler):
     def setQuery(self, query: str) -> None:
         self.__query = query
 
-    def get_table_records(self, parameters: dict | None, table_name: str, join_condition: str = "", filter_condition: str = "", column_names: str = "*", is_sorted: bool = False, sort_condition: str = "") -> list:
+    def getParameters(self) -> tuple | None:
+        return self.__parameters
+
+    def setParameters(self, parameters: tuple | None) -> None:
+        self.__parameters = parameters
+
+    def get_table_records(self, parameters: list | None, table_name: str, join_condition: str = "", filter_condition: str = "", column_names: str = "*", is_sorted: bool = False, sort_condition: str = "") -> list:
         """
         Retrieving data from the database.  (SELECT)
 
         Parameters:
-            parameters:         object|null:    The parameters to be passed into the query.
-            table_name:         string:         The name of the table.
-            column_names:       string:         The name of the columns.
-            join_condition      string:         Joining table condition.
-            filter_condition    string:         Items to be filtered with.
-            is_sorted           boolean:        The condition to sort the result set.
-            sort_condition      string:         The items to be sorted.
+            parameters:         array|null: The parameters to be passed into the query.
+            table_name:         string:     The name of the table.
+            column_names:       string:     The name of the columns.
+            join_condition      string:     Joining table condition.
+            filter_condition    string:     Items to be filtered with.
+            is_sorted           boolean:    The condition to sort the result set.
+            sort_condition      string:     The items to be sorted.
 
         Returns:
             array: The list of data retrieved.
@@ -47,11 +61,11 @@ class Object_Relational_Mapper(Database_Handler):
         query = f"SELECT {column_names} FROM {table_name}"
         self.setQuery(query)
         self._get_join(join_condition)
-        self._get_filter(filter_condition)
+        self._get_filter(filter_condition, parameters)
         if is_sorted == True:
             query = f"{self.getQuery()} ORDER BY {sort_condition}"
             self.setQuery(query)
-        self.query(self.getQuery(), parameters)
+        self.query(self.getQuery(), self.getParameters())
         self.execute()
         return self.resultSet()
 
@@ -71,12 +85,13 @@ class Object_Relational_Mapper(Database_Handler):
             query = f"{self.getQuery()} JOIN {condition}"
         self.setQuery(query)
 
-    def _get_filter(self, condition: str):
+    def _get_filter(self, condition: str, parameters: list | None) -> None:
         """
         Bulding the query needed for retrieving specific data.
 
         Parameters:
-            condition:  string: The WHERE statement that will be used.
+            condition:  string:     The WHERE statement that will be used.
+            parameters: array|null: Parameters to be used to filter the data.
 
         Returns: void
         """
@@ -84,4 +99,8 @@ class Object_Relational_Mapper(Database_Handler):
             query = self.getQuery()
         else:
             query = f"{self.getQuery()} WHERE {condition}"
+            if type(parameters) is list:
+                self.setParameters(tuple(parameters))
+            else:
+                self.setParameters(None)
         self.setQuery(query)

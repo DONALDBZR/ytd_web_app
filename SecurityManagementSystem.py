53a8bfbd3ecb74d78eb77fe689a28db28f5275f5
DONALDBZR <andygaspard@hotmail.com>
2023-08-22 18:29:28 +0400

    UPDATE [0.4.1] It will be a major component that will assure the security of the data that will be stored across the application.

---
+++ SecurityManagementSystem.py
@@ -0,0 +1,7 @@
+from ObjectRelationalMapper import Object_Relational_Mapper
+
+
+class Security_Management_System:
+    """
+    It will be a major component that will assure the security of the data that will be stored across the application.
+    """

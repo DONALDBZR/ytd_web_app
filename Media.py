0e04856c857bf6c7ea8335fa87b25fe1b51c434b
DONALDBZR <andygaspard@hotmail.com>
2023-08-24 00:08:38 +0400

    UPDATE [0.5.1] It allows the application to manage the media.

---
+++ Media.py
@@ -0,0 +1,187 @@
+class Media:
+    """
+    It allows the application to manage the media.
+    """
+    __search: str
+    """
+    The uniform resource locator to be searched.
+
+    Type: string
+    Visibility: private
+    """
+    _YouTubeDownloader: None
+    """
+    It will handle every operations related to YouTube
+
+    Type: YouTube_Downloader
+    Visibility: protected
+    """
+    __referer: str | None
+    """
+    The http referrer which is the uniform resource locator that is needed to be able to allow the user to download the required media.
+
+    Type: string|null
+    Visibility: private
+    """
+    __database_handler: None
+    """
+    The database handler that will communicate with the database server.
+
+    Type: Database_Handler
+    Visibility: private
+    """
+    __identifier: int
+    """
+    The identifier of the required media
+
+    Type: int
+    Visibility: private
+    """
+    __value: str
+    """
+    The value of the required media which have to correspond to the name of the platform from which the media comes from.
+
+    Type: string | null
+    Visibility: private
+    """
+    __timestamp: str
+    """
+    The timestamp at which the session has been created
+
+    Type: string
+    Visibility: private
+    """
+
+    def __init__(self, search: str, referer: str | None, value: str) -> None:
+        """
+        Instantiating the media's manager which will interact with the media's dataset and do the required processing.
+
+        Parameters:
+            search: string: The uniform resource locator to be searched.
+            referer: string | null: The http referrer which is the uniform resource locator that is needed to be able to allow the user to download the required media.
+            value: string: The value of the required media which have to correspond to the name of the platform from which the media comes from.
+        """
+        self.setDatabaseHandler(Database_Handler())
+        self.getDatabaseHandler().query(
+            "CREATE TABLE IF NOT EXISTS `Media` (identifier INT PRIMARY KEY AUTO_INCREMENT, `value` VARCHAR(8))", None)
+        self.getDatabaseHandler().execute()
+        self.setSearch(search)
+        self.setReferer(referer)
+        self.setValue(value)
+
+    def getSearch(self) -> str:
+        return self.__search
+
+    def setSearch(self, search: str) -> None:
+        self.__search = search
+
+    def getReferer(self) -> str | None:
+        return self.__referer
+
+    def setReferer(self, referer: str | None) -> None:
+        self.__referer = referer
+
+    def getDatabaseHandler(self):
+        return self.__database_handler
+
+    def setDatabaseHandler(self, database_handler) -> None:
+        self.__database_handler = database_handler
+
+    def getIdentifier(self) -> int:
+        return self.__identifier
+
+    def setIdentifier(self, identifier: int) -> None:
+        self.__identifier = identifier
+
+    def getValue(self) -> str:
+        return self.__value
+
+    def setValue(self, value: str) -> None:
+        self.__value = value
+
+    def getTimestamp(self) -> str:
+        return self.__timestamp
+
+    def setTimestamp(self, timestamp: str) -> None:
+        self.__timestamp = timestamp
+
+    def verifyPlatform(self) -> dict:
+        """
+        Verifying the uniform resource locator in order to switch to the correct system as well as select and return the correct response.
+
+        Returns: object
+        """
+        response = {}
+        media = self.getMedia()
+        # Verifying that the media does not exist to create one.
+        if media["status"] != 200:
+            self.postMedia()
+            self.verifyPlatform()
+        else:
+            self.setIdentifier(media["data"][0][0])
+        # Verifying the platform data to redirecto to the correct system.
+        if "youtube" in self.getValue():
+            response = {
+                "status": 200,
+                "data": self.handleYouTube()
+            }
+        return response
+
+    def getMedia(self) -> dict:
+        """
+        Retrieving the Media data from the Media table.
+
+        Returns: object
+        """
+        self.getDatabaseHandler().query(
+            "SELECT * FROM `Media` WHERE value = %s", (self.getValue(),))
+        media = self.getDatabaseHandler().resultSet()
+        self.setTimestamp(datetime.now().strftime("%Y-%m-%d - %H:%M:%S"))
+        response = {}
+        if len(media) == 0:
+            response = {
+                'status': 204,
+                'data': media,
+                'timestamp': self.getTimestamp()
+            }
+        else:
+            response = {
+                'status': 200,
+                'data': media,
+                'timestamp': self.getTimestamp()
+            }
+        return response
+
+    def postMedia(self) -> None:
+        """
+        Creating a record for the media with its data.
+
+        Returns: void
+        """
+        self.getDatabaseHandler().query(
+            "INSERT INTO `Media` (`value`) VALUES (%s)", (self.getValue(),))
+        self.getDatabaseHandler().execute()
+
+    def handleYouTube(self) -> dict:
+        """
+        Handling the data throughout the You Tube Downloader which will depend on the referer.
+
+        Returns: object
+        """
+        self._YouTubeDownloader = YouTube_Downloader(
+            self.getSearch(), self.getIdentifier())
+        response = {}
+        # Verifying the referer to retrieve to required data
+        if self.getReferer() is None:
+            session["Media"] = {}
+            session["Media"]["YouTube"] = self._YouTubeDownloader.search()
+            filename = "./Cache/Session/Users/" + \
+                session["Client"]["ip_address"] + ".json"
+            file = open(filename, "w")
+            file.write(json.dumps(session, indent=4))
+            file.close()
+            response = {
+                "status": 200,
+                "data": self._YouTubeDownloader.search()
+            }
+        return response

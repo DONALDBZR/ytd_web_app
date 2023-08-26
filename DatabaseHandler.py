9f5a67efe85fe0b4d3dc54e98580ae2c7d90ec64
DONALDBZR <andygaspard@hotmail.com>
2023-08-26 11:32:08 +0400

    UPDATE [0.7.46] Updating a specific table in the database.

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -266,3 +266,22 @@ class Database_Handler:
         self.setParameters(parameters)
         self._query(self.getQuery(), self.getParameters())
         self._execute()
+
+    def update_data(self, table: str, values: str, parameters: tuple | None, condition: str = "") -> None:
+        """
+        Updating a specific table in the database.
+
+        Parameters:
+            table:      string: Table
+            values:     string: Columns to be modified and data to be put within
+            condition:  string: Condition for the data to be modified
+            parameters: array:  Data to be used for data manipulation.
+
+        Returns: void
+        """
+        query = f"UPDATE {table} SET {values}"
+        self.setQuery(query)
+        self.setParameters(parameters)
+        self._get_filter(condition)
+        self._query(self.getQuery(), self.getParameters())
+        self._execute()
--- ObjectRelationalMapper.py
+++ ObjectRelationalMapper.py
@@ -14,24 +14,6 @@ class Object_Relational_Mapper(Database_Handler):
         super().__init__()
         self.setParameters(None)
 
-    def update_data(self, table: str, values: str, parameters: tuple | None, condition: str = "") -> None:
-        """
-        Updating a specific table in the database.
-
-        Parameters:
-            table:      string: Table
-            values:     string: Columns to be modified and data to be put within
-            condition:  string: Condition for the data to be modified
-            parameters: array:  Data to be used for data manipulation.
-
-        Returns: void
-        """
-        query = f"UPDATE {table} SET {values}"
-        self.setQuery(query)
-        self._get_filter(condition, parameters)
-        self.query(self.getQuery(), self.getParameters())
-        self.execute()
-
     def delete_records(self, table: str, parameters: tuple | None, condition: str = "") -> None:
         """
         Deleting data from the database.

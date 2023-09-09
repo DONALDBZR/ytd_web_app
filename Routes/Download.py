81695e1eb9ac9f07d43a796866a72d5f253ece1f
DONALDBZR <andygaspard@hotmail.com>
2023-09-09 16:35:13 +0400

    [UPDATE 0.16.12] Rendering the template needed which will import the web-worker.

--- Routes/Download.py
+++ Routes/Download.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Response, render_template
 
 Download_Portal = Blueprint("Download", __name__)
 """
@@ -6,3 +6,20 @@ The Routing for all the Downloads.
 
 Type: Blueprint
 """
+
+
+@Download_Portal.route('/YouTube/<string:identifier>', methods=['GET'])
+def downloadPage(identifier: str) -> Response:
+    """
+    Rendering the template needed which will import the
+    web-worker.
+
+    Parameters:
+        identifier: string: Identifier of the media content to be searched.
+
+    Returns: Response
+    """
+    template = render_template('page.html')
+    mime_type = "text/html"
+    status = 200
+    return Response(template, status, mimetype=mime_type)

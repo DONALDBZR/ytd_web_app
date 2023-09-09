f4811f32255b370b777a8198b4b6fa7acc5849cd
DONALDBZR <andygaspard@hotmail.com>
2023-09-09 16:15:28 +0400

    [UPDATE 0.16.03] Allowing the Session Manager to update the session.

--- Routes/Session.py
+++ Routes/Session.py
@@ -10,7 +10,7 @@ Type: Blueprint
 """
 
 
-@Session_Portal.route('/Session', methods=['GET'])
+@Session_Portal.route('/', methods=['GET'])
 def getSession() -> Response:
     """
     Sending the session data in the form of JSON.
@@ -33,4 +33,32 @@ def getSession() -> Response:
     response = json.dumps(session_data, indent=4)
     mime_type = "application/json"
     status = 200
+    return Response(response, status, mimetype=mime_type)
+
+
+@Session_Portal.route('/Post', methods=['POST'])
+def setSession() -> Response:
+    """
+    Allowing the Session Manager to update the session.
+
+    Returns: Response
+    """
+    payload = request.json
+    user_request = {
+        "ip_address": str(request.environ.get('REMOTE_ADDR')),
+        "http_client_ip_address": str(request.environ.get("HTTP_CLIENT_IP")),
+        "proxy_ip_address": str(request.environ.get("HTTP_X_FORWARDED_FOR")),
+        "port": int(request.environ.get("SERVER_PORT"))  # type: ignore
+    }
+    SessionManager = Session_Manager(user_request, session)
+    SessionManager.updateSession(payload)
+    mime_type = "application/json"
+    status = 201
+    session_data = {
+        "Client": {
+            "timestamp": SessionManager.getSession()["Client"]["timestamp"],
+            "color_scheme": SessionManager.getSession()["Client"]["color_scheme"]
+        }
+    }
+    response = json.dumps(session_data, indent=4)
     return Response(response, status, mimetype=mime_type)

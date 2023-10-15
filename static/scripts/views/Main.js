024588d1d32560df0ff5fdc4e3cda478e57e8676
DONALDBZR <andygaspard@hotmail.com>
2023-10-15 13:16:45 +0400

    [UPDATE 1.0.42] Restructuring the application for it to be RESTful

--- static/scripts/views/Main.js
+++ static/scripts/views/Main.js
@@ -55,6 +55,7 @@ class Main extends React.Component {
                 })
             );
     }
+
     /**
      * Running the methods needed as soon as the component has been successfully mounted.
      * @returns {void}
@@ -62,6 +63,7 @@ class Main extends React.Component {
     componentDidMount() {
         this.getRoute();
     }
+
     /**
      * Setting the view route of the application.
      * @returns {void}
@@ -74,6 +76,7 @@ class Main extends React.Component {
             },
         }));
     }
+
     /**
      * Handling any change that is made in the user interface
      * @param {Event} event
@@ -90,6 +93,7 @@ class Main extends React.Component {
             },
         }));
     }
+
     /**
      * Handling the form submission
      * @param {Event} event
@@ -119,6 +123,7 @@ class Main extends React.Component {
             .then(() => this.setRoute())
             .then(() => this.redirector(delay, this.state.System.url));
     }
+
     /**
      * Generating the uniform resource locator for retrieving the
      * metadata of the content.
@@ -137,6 +142,7 @@ class Main extends React.Component {
             )}`;
         }
     }
+
     /**
      * Setting the metadata into the state of the application.
      * @param {object} data Metadata
@@ -199,6 +205,7 @@ class Main extends React.Component {
             }));
         }
     }
+
     /**
      * Retrieving the data of the media content that is searched by
      * the user.
@@ -215,6 +222,7 @@ class Main extends React.Component {
                     (document.querySelector("#loading").style.display = "none")
             );
     }
+
     /**
      * Retrieving Media from the server by using its uniform
      * resource locator.
@@ -247,6 +255,7 @@ class Main extends React.Component {
                 }, delay);
             });
     }
+
     /**
      * Redirecting the user to an intended url
      * @param {number} delay The amount of time in milliseconds before firing the method
@@ -258,6 +267,7 @@ class Main extends React.Component {
             window.location.href = uniform_resource_locator;
         }, delay);
     }
+
     /**
      * Setting the uniform resource locator for a specific YouTube content.
      * @param {object} data The dataset from the server.
@@ -275,6 +285,7 @@ class Main extends React.Component {
             },
         }));
     }
+
     /**
      * Extracting the identifier of a specific YouTube content.
      * @returns {void}
@@ -313,6 +324,7 @@ class Main extends React.Component {
             }));
         }
     }
+
     /**
      * Setting the route to be redirected.
      * @returns {void}
@@ -335,6 +347,7 @@ class Main extends React.Component {
             }));
         }
     }
+
     /**
      * Downloading the file retrieved from the server.
      * @param {Event} event
@@ -375,6 +388,7 @@ class Main extends React.Component {
                 a.click();
             });
     }
+
     /**
      * Checking that the location of the media file needed is in
      * the state of the application.
@@ -388,6 +402,7 @@ class Main extends React.Component {
             window.location.href = `/Search/${this.state.Media.YouTube.identifier}`;
         }
     }
+
     /**
      * Retrieving the media file for the application to load.
      * @returns {string}
@@ -406,6 +421,7 @@ class Main extends React.Component {
             );
         }
     }
+
     /**
      * Rendering the component
      * @returns {HTMLElement}
@@ -420,6 +436,7 @@ class Main extends React.Component {
         }
     }
 }
+
 /**
  * The component to be rendered for the homepage
  */
@@ -431,9 +448,7 @@ class Homepage extends Main {
     constructor(props) {
         super(props);
     }
-    componentDidMount() {
-        this.getTrend();
-    }
+
     /**
      * Rendering the component
      * @returns {HTMLElement}
@@ -457,6 +472,7 @@ class Homepage extends Main {
         );
     }
 }
+
 /**
  * The component to be rendered for the trends
  */
@@ -468,6 +484,7 @@ class Trend extends Homepage {
     constructor(props) {
         super(props);
     }
+
     /**
      * Rendering the component
      * @returns {HTMLDivElement}
@@ -522,6 +539,7 @@ class Trend extends Homepage {
         );
     }
 }
+
 /**
  * The component to be rendered for the search page
  */
@@ -533,6 +551,7 @@ class Search extends Main {
     constructor(props) {
         super(props);
     }
+
     /**
      * Rendering the component
      * @returns {HTMLMainElement}
@@ -573,20 +592,17 @@ class Media extends Search {
     constructor(props) {
         super(props);
     }
-    /**
-     * Running the methods needed as soon as the component has been
-     * successfully mounted.
-     * @returns {void}
-     */
-    componentDidMount() {
-        this.getRoute();
-        setTimeout(() => {
-            if (this.state.System.view_route != "/Search/") {
-                document.querySelector("#loading").style.display = "flex";
-                this.getMedia();
-            }
-        }, 1);
-    }
+
+    // componentDidMount() {
+    //     this.getRoute();
+    //     setTimeout(() => {
+    //         if (this.state.System.view_route != "/Search/") {
+    //             document.querySelector("#loading").style.display = "flex";
+    //             this.getMedia();
+    //         }
+    //     }, 1);
+    // }
+
     /**
      * Rendering the component
      * @returns {HTMLDivElement}
@@ -604,6 +620,7 @@ class Media extends Search {
         }
     }
 }
+
 /**
  * The component that is the YouTube component given that it will only be rendered only when the data corresponds to it.
  */
@@ -615,6 +632,7 @@ class YouTube extends Media {
     constructor(props) {
         super(props);
     }
+
     /**
      * Rendering the component
      * @returns {HTMLDivElement}
@@ -671,6 +689,7 @@ class YouTube extends Media {
         );
     }
 }
+
 /**
  * The component to be rendered for the Download page
  */
@@ -682,6 +701,7 @@ class Download extends Main {
     constructor(props) {
         super(props);
     }
+
     /**
      * Rendering the component
      * @returns {HTMLMainElement}
@@ -696,6 +716,7 @@ class Download extends Main {
         }
     }
 }
+
 /**
  * The component to be rendered for the Download page but only
  * when it is for media from YouTube
@@ -708,18 +729,16 @@ class YouTubeDownloader extends Main {
     constructor(props) {
         super(props);
     }
-    /**
-     * Running the methods needed as soon as the component has been successfully mounted
-     * @returns {void}
-     */
-    componentDidMount() {
-        this.getRoute();
-        setTimeout(() => {
-            if (window.location.pathname != "/Search/") {
-                this.getMedia();
-            }
-        }, 1);
-    }
+
+    // componentDidMount() {
+    //     this.getRoute();
+    //     setTimeout(() => {
+    //         if (window.location.pathname != "/Search/") {
+    //             this.getMedia();
+    //         }
+    //     }, 1);
+    // }
+
     /**
      * Rendering the component
      * @returns {HTMLDivElement}

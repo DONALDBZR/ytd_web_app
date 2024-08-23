39cea58ef8e96621dc7a3f8da4ddf8338c980c7d
DONALDBZR <andygaspard@hotmail.com>
2024-08-23 20:46:40 +0400

    [UPDATE 2.10.107] Constructing that application from the React's Component

--- static/scripts/views/Homepage.js
+++ static/scripts/views/Homepage.js
@@ -2,7 +2,22 @@
  * The component that is the homepage of the application.
  */
 class Homepage extends React.Component {
-    
+    /**
+     * Constructing that application from the React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+        /**
+         * The states of the application
+         * @type {{System: {view_route: string}}}
+         */
+        this.state = {
+            System: {
+                view_route: "",
+            },
+        };
+    }
 }
 
 // Rendering the page

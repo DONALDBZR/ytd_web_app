f92c58c373d25145ebdb7c52b971bd086bf356c6
DONALDBZR <andygaspard@hotmail.com>
2024-08-23 22:16:09 +0400

    [UPDATE 2.10.137] Rendering the component

--- static/scripts/views/Homepage.js
+++ static/scripts/views/Homepage.js
@@ -376,8 +376,24 @@ class Main extends Homepage {
 
     /**
      * Rendering the component
-     * 
+     * @returns {HTMLMainElement}
      */
+    render() {
+        return (
+            <main>
+                <div id="loading">
+                    <i class="fa-solid fa-spinner fa-spin"></i>
+                </div>
+                <p> The aim of the application is that software and contents must be free and it allows anyone to get content from certain platforms to be obtained for free as it is an application developed for people by people.</p>
+                <div>
+                    <div>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+                </div>
+                <Trend data={trend} />
+            </main>
+        );
+    }
 }
 
 // Rendering the page

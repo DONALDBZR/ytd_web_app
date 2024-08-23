6d43d1d9f88f9b6d910cc6c4ec8aaa590496ed04
DONALDBZR <andygaspard@hotmail.com>
2024-08-23 22:00:19 +0400

    [UPDATE 2.10.127] Extracting the identifier of a specific YouTube content.

--- static/scripts/views/Homepage.js
+++ static/scripts/views/Homepage.js
@@ -171,6 +171,46 @@ class Header extends Homepage {
     }
 
     /**
+     * Extracting the identifier of a specific YouTube content.
+     * @param {string} platform The platform to be searched on.
+     * @param {string} search The search data to be searched.
+     * @returns {Promise<number>}
+     */
+    async setMediaYouTubeIdentifier(platform, search) {
+        const response = await this.setMediaYouTubeUniformResourceLocator(platform, search);
+        if (this.state.Media.YouTube.uniform_resource_locator.includes("youtube")) {
+            this.setState((previous) => ({
+                Media: {
+                    ...previous.Media,
+                    YouTube: {
+                        ...previous.Media.YouTube,
+                        identifier: this.state.Media.YouTube.uniform_resource_locator.replace(
+                            "https://www.youtube.com/watch?v=",
+                            ""
+                        )
+                        .replace(/\?.*/, ""),
+                    },
+                },
+            }));
+        } else {
+            this.setState((previous) => ({
+                Media: {
+                    ...previous.Media,
+                    YouTube: {
+                        ...previous.Media.YouTube,
+                        identifier: this.state.Media.YouTube.uniform_resource_locator.replace(
+                            "https://youtu.be/",
+                            ""
+                        )
+                        .replace(/\?.*/, ""),
+                    },
+                },
+            }));
+        }
+        return response;
+    }
+
+    /**
      * Rendering the component
      * @returns {HTMLHeaderElement}
      */

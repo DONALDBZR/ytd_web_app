1b636856309e7c0d2d39ce1b5352d4543868216a
DONALDBZR <andygaspard@hotmail.com>
2024-08-23 22:23:54 +0400

    [UPDATE 2.10.144] Constructing the Trend component which is based on the Main component of the Homepage.

--- static/scripts/views/Homepage.js
+++ static/scripts/views/Homepage.js
@@ -454,5 +454,27 @@ class Main extends Homepage {
     }
 }
 
+/**
+ * The component to be rendered for the trends.
+ */
+class Trend extends Main {
+    /**
+     * Constructing the Trend component which is based on the Main
+     * component of the Homepage.
+     * @param {{data: {Trend: [{uniform_resource_locator: string, author: string, title: string, identifier: string, author_channel: string, views: number, published_at: string, thumbnail: string, duration: string, audio_file: string, video_file: string}]}}} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+        this.props = props.data;
+        /**
+         * The state of the component.
+         * @type {{Trend: [{uniform_resource_locator: string, author: string, title: string, identifier: string, author_channel: string, views: number, published_at: string, thumbnail: string, duration: string, audio_file: string, video_file: string}]}}
+         */
+        this.state = {
+            Trend: this.props.Trend,
+        };
+    }
+}
+
 // Rendering the page
 ReactDOM.render(<Homepage />, document.body);

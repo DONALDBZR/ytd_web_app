b6865d28d49833dd9260027e15514210939bc86e
DONALDBZR <andygaspard@hotmail.com>
2024-08-23 20:44:31 +0400

    [UPDATE 2.10.106] The component that is the homepage of the application.

---
+++ static/scripts/views/Homepage.js
@@ -0,0 +1,9 @@
+/**
+ * The component that is the homepage of the application.
+ */
+class Homepage extends React.Component {
+    
+}
+
+// Rendering the page
+ReactDOM.render(<Homepage />, document.body);

e504ddb724296be77887ba7fa22481a45a6ed6dd
DONALDBZR <andygaspard@hotmail.com>
2024-09-10 21:53:54 +0400

    [UPDATE 2.10.234] The component for the footer of the Search Portal

---
+++ static/scripts/Components/Search/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+
+/**
+ * The component that is the footer for the pages
+ */
+class Footer extends Component {
+    /**
+     * Rendering the component
+     * @returns {HTMLFooterElement}
+     */
+    render() {
+        return (
+            <footer>
+                <div>Extractio</div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;

9072a98572264664b2a2b31882bd945fbb795f62
DONALDBZR <andygaspard@hotmail.com>
2024-10-07 12:26:42 +0400

    [UPDATE 2.11.42] The component that will be used for the Search page that will have the data for the Media.

---
+++ static/scripts/views/Main/Media.js
@@ -0,0 +1,82 @@
+/**
+ * The component that will be used for the Search page that
+ * will have the data for the Media.
+ */
+class Media extends React.Component {
+    /**
+     * Constructing the component that will be used for the Media
+     * data type.
+     * @param {{data: {System: {dom_element: HTMLElement}}}} props
+     */
+    constructor(props) {
+        super(props);
+        this.props = props;
+        /**
+         * The states of the application.
+         * @type {{System: {dom_element: HTMLElement}}}
+         */
+        this.state = {
+            System: {
+                dom_element: this.props.data.System.dom_element,
+            },
+        };
+    }
+
+    /**
+     * Running the methods needed as soon as the component has been
+     * successfully mounted.
+     * @returns {void}
+     */
+    componentDidMount() {
+        this.setData();
+        console.info(`Route: ${window.location.pathname}\nComponent: Main.Search.Media\nComponent Status: Mount`);
+    }
+
+    /**
+     * Setting the main state of the component.
+     * @returns {void}
+     */
+    setData() {
+        this.setState((previous) => ({
+            ...previous,
+            System: {
+                ...previous.System,
+                dom_element: this.props.data.System.dom_element,
+            },
+        }));
+    }
+
+    /**
+     * Updating the component as soon as the states are different.
+     * @param {{data: {System: {dom_element: HTMLElement}}}} previous_props The properties of the component
+     * @returns {void}
+     */
+    componentDidUpdate(previous_props) {
+        if (this.props != previous_props) {
+            this.setData();
+        }
+        console.info(`Route: ${window.location.pathname}\nComponent: Main.Search.Media\nComponent Status: Update`);
+    }
+
+    /**
+     * Rendering the component
+     * @returns {React.Component}
+     */
+    render() {
+        /**
+         * The properties of the Media component.
+         * @type {{System: {dom_element: HTMLElement}}}
+         */
+        const media = {
+            System: {
+                dom_element: this.state.System.dom_element,
+            },
+        };
+        return (
+            <div className="Media">
+                <YouTube data={media} />
+                <RelatedContents data={media} />
+            </div>
+        );
+    }
+}

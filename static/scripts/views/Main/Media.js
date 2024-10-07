95b87738ce964d1c4eadd53719f5eea24fece9ae
DONALDBZR <andygaspard@hotmail.com>
2024-10-07 12:59:12 +0400

    [UPDATE 2.11.47] Constructing the component that will be used for the Media data type.

--- static/scripts/views/Main/Media.js
+++ static/scripts/views/Main/Media.js
@@ -10,33 +10,6 @@ class Media extends React.Component {
      */
     constructor(props) {
         super(props);
-        /**
-         * The states of the application.
-         * @type {{}}
-         */
-        this.state = {};
-    }
-
-    /**
-     * Running the methods needed as soon as the component has been
-     * successfully mounted.
-     * @returns {void}
-     */
-    componentDidMount() {
-        this.setData();
-        console.info(`Route: ${window.location.pathname}\nComponent: Search.Main.MainSearch.Media\nComponent Status: Mount`);
-    }
-
-    /**
-     * Setting the main state of the component.
-     * @returns {void}
-     */
-    setData() {
-        const media = JSON.parse(localStorage.getItem("media")).data.Media;
-        this.setData((previous) => ({
-            ...previous,
-            Media: media,
-        }));
     }
 
     /**

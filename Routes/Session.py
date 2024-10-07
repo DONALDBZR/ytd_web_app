dd5dd22cf437390c758df0f8eae5cb2800e3c4af
DONALDBZR <andygaspard@hotmail.com>
2024-10-07 19:34:36 +0400

    [UPDATE 2.11.66] Allowing the Session Manager to update the session.

--- Routes/Session.py
+++ Routes/Session.py
@@ -45,22 +45,21 @@ def setSession() -> Response:
     Returns:
         Response
     """
-    payload = request.json
-    user_request = {
+    payload: Dict[str, Dict[str, str]] = request.json # type: ignore
+    mime_type: str = "application/json"
+    status: int = 202
+    user_request: Dict[str, str] = {
         "ip_address": str(request.environ.get('REMOTE_ADDR')),
         "http_client_ip_address": str(request.environ.get("HTTP_CLIENT_IP")),
         "proxy_ip_address": str(request.environ.get("HTTP_X_FORWARDED_FOR")),
-        "port": str(request.environ.get("SERVER_PORT"))  # type: ignore
+        "port": str(request.environ.get("SERVER_PORT"))
     }
-    SessionManager = Session_Manager(user_request, session)
-    SessionManager.updateSession(payload)  # type: ignore
-    mime_type = "application/json"
-    status = 202
-    session_data = {
+    SessionManager: Session_Manager = Session_Manager(user_request, session)
+    SessionManager.updateSession(payload)
+    session_data: Dict[str, Dict[str, Union[int, str]]] = {
         "Client": {
-            "timestamp": SessionManager.getSession()["Client"]["timestamp"],
-            "color_scheme": SessionManager.getSession()["Client"]["color_scheme"]
+            "timestamp": int(SessionManager.getSession()["Client"]["timestamp"]),
+            "color_scheme": str(SessionManager.getSession()["Client"]["color_scheme"])
         }
     }
-    response = json.dumps(session_data, indent=4)
-    return Response(response, status, mimetype=mime_type)
+    return Response(dumps(session_data, indent=4), status, mimetype=mime_type)

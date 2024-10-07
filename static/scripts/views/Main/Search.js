956fcf15ffc73be42a96b840a4365f71784b3bd9
DONALDBZR <andygaspard@hotmail.com>
2024-10-07 13:44:11 +0400

    [UPDATE 2.11.52] The component to be rendered for the search page

--- static/scripts/views/Main/Search.js
+++ static/scripts/views/Main/Search.js
@@ -8,36 +8,7 @@ class MainSearch extends React.Component {
      */
     constructor(props) {
         super(props);
-        // /**
-        //  * The data for the properties of the Search component.
-        //  * @type {{Media: {YouTube: {uniform_resource_locator: string, author: string, title: string,identifier: string, author_channel: string, views: number, published_at: string, thumbnail: string, duration: string,audio_file: string, video_file: string}}}}
-        //  */
-        // this.state = {
-        //     Media: {},
-        // };
     }
-
-    // /**
-    //  * Running the methods needed as soon as the component has been
-    //  * successfully mounted.
-    //  * @returns {void}
-    //  */
-    // componentDidMount() {
-    //     this.setData();
-    //     console.info(`Route: ${window.location.pathname}\nComponent: Search.Main.MainSearch\nComponent Status: Mount`);
-    // }
-
-    // /**
-    //  * Setting the main state of the component.
-    //  * @returns {void}
-    //  */
-    // setData() {
-    //     const media = JSON.parse(localStorage.getItem("media")).data.Media;
-    //     this.setData((previous) => ({
-    //         ...previous,
-    //         Media: media,
-    //     }));
-    // }
 
     /**
      * Rendering the component

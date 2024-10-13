933289beb28df5bbb39f68a714015aa55ffa0171
DONALDBZR <andygaspard@hotmail.com>
2024-10-13 16:59:58 +0400

    [UPDATE 2.11.104] Retrieving the video file and saving it on the server as well as adding its meta data in the database.

--- Models/YouTubeDownloader.py
+++ Models/YouTubeDownloader.py
@@ -436,27 +436,16 @@ class YouTube_Downloader:
         Retrieving the video file and saving it on the server as
         well as adding its meta data in the database.
 
-        Return:
-            (string)
+        Returns:
+            string
         """
-        response: str
-        for index in range(0, len(self.getStreams().filter(mime_type="video/mp4", audio_codec="mp4a.40.2", resolution="720p")), 1):
-            self.setITAG(
-                self.getStreams().filter(
-                    mime_type="video/mp4",
-                    audio_codec="mp4a.40.2",
-                    resolution="720p"
-                )[index].itag
-            )
-        self.setStream(
-            self.getStreams().get_by_itag(self.getITAG())
-        )
-        self.setMimeType("video/mp4")
-        if type(self.getStream()) is Stream:
-            response = self.__downloadVideo()
-        else:
-            response = ""
-        return response
+        streams: List[Stream] = [stream for stream in self.getStreams().filter(mime_type="video/mp4", audio_codec="mp4a.40.2", resolution="720p")]
+        if streams:
+            self.setITAG(streams[0].itag)
+            self.setStream(self.getStreams().get_by_itag(self.getITAG()))
+            self.setMimeType("video/mp4")
+            return self.__downloadVideo()
+        return ""
 
     def __downloadVideo(self) -> str:
         """

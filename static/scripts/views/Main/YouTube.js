107c84546ee4aa393c665d982b7bebfa5afbcf09
DONALDBZR <andygaspard@hotmail.com>
2024-12-02 23:33:54 +0400

    [UPDATE 2.11.239] Rendering the component

--- static/scripts/views/Main/YouTube.js
+++ static/scripts/views/Main/YouTube.js
@@ -144,7 +144,7 @@ class YouTube extends React.Component {
                 </div>
                 <div class="data">
                     <div class="metadata">
-                        <div>{this.state.Media.YouTube.title}</div>
+                        <div style={{height: this.getTitleHeight(this.state.Media.YouTube.title)}}>{this.state.Media.YouTube.title}</div>
                         <div>
                             <a href={this.state.Media.YouTube.author_channel} target="__blank">{this.state.Media.YouTube.author}</a>
                         </div>

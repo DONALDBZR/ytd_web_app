ef7498135513cdeab709936277c442b935f003a7
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 23:21:04 +0400

    [UPDATE 2.12.36] Rendering the component for the header.

--- static/scripts/views/Header.js
+++ static/scripts/views/Header.js
@@ -17,8 +17,8 @@ class Header extends React.Component {
     render() {
         if (window.location.pathname.includes("Search")) {
             return <HeaderSearch />;
-        // } else if (window.location.pathname.includes("Download")) {
-        //     return <Download />;
+        } else if (window.location.pathname.includes("Download")) {
+            return <HeaderDownload />;
         } else {
             return <HeaderHomepage />;
         }

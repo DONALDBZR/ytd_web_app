2e8aeac508d5adf6da3ef12d7504007dac90a0bc
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 20:26:23 +0400

    [UPDATE 2.10.28] Sending the session data in the form of JSON.

--- Routes/Session.py
+++ Routes/Session.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, Response, request, session
 from Models.SessionManagementSystem import Session_Manager
+from typing import Dict, Union
+from json import dumps
 import json
 
 
@@ -16,24 +18,25 @@ def getSession() -> Response:
     """
     Sending the session data in the form of JSON.
 
-    Returns: Response
+    Returns:
+        Response
     """
-    user_request = {
+    mime_type: str = "application/json"
+    status: int = 200
+    user_request: Dict[str, str] = {
         "ip_address": str(request.environ.get('REMOTE_ADDR')),
         "http_client_ip_address": str(request.environ.get("HTTP_CLIENT_IP")),
         "proxy_ip_address": str(request.environ.get("HTTP_X_FORWARDED_FOR")),
         "port": str(request.environ.get("SERVER_PORT"))
     }
-    SessionManager = Session_Manager(user_request, session)
-    session_data = {
+    SessionManager: Session_Manager = Session_Manager(user_request, session)
+    session_data: Dict[str, Dict[str, Union[int, str]]] = {
         "Client": {
-            "timestamp": SessionManager.getSession()["Client"]["timestamp"],
-            "color_scheme": SessionManager.getSession()["Client"]["color_scheme"]
+            "timestamp": int(SessionManager.getSession()["Client"]["timestamp"]),
+            "color_scheme": str(SessionManager.getSession()["Client"]["color_scheme"])
         }
     }
-    response = json.dumps(session_data, indent=4)
-    mime_type = "application/json"
-    status = 200
+    response: str = dumps(session_data, indent=4)
     return Response(response, status, mimetype=mime_type)
 
 

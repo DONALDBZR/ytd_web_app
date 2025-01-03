7801cc366aa08b36cf4a5ab2c5d6ce5b8b91fc13
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 21:38:20 +0400

    [UPDATE 2.12.12] Deleting data from the database.

--- Models/DatabaseHandler.py
+++ Models/DatabaseHandler.py
@@ -242,7 +242,7 @@ class Database_Handler:
         self._query(self.getQuery(), self.getParameters())
         self._execute()
 
-    def delete_data(self, table: str, parameters: Union[Tuple[Any], None], condition: str = "") -> None:
+    def deleteData(self, table: str, parameters: Union[Tuple[Any], None], condition: str = "") -> None:
         """
         Deleting data from the database.
 

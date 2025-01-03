3023d772eb8925f1c972b77e4c0549431faa5204
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 23:34:15 +0400

    [UPDATE 2.12.40] The component to be rendered for the Download page

---
+++ static/scripts/views/Main/Download.js
@@ -0,0 +1,27 @@
+/**
+ * The component to be rendered for the Download page
+ */
+class MainDownload extends React.Component {
+    /**
+     * Constructing the application from React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+    }
+
+    /**
+     * Rendering the component
+     * @returns {React.Component}
+     */
+    render() {
+        return <YouTubeDownloader />;
+        if (this.state.System.view_route.includes("YouTube")) {
+            return (
+                <>
+                    <YouTubeDownloader />
+                </>
+            );
+        }
+    }
+}

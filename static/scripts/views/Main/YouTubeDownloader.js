c97b4d38883cd39af8feac81a392f004e680aaa9
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 23:39:39 +0400

    [UPDATE 2.12.45] The component to be rendered for the Download page but only when it is for media from YouTube.

---
+++ static/scripts/views/Main/YouTubeDownloader.js
@@ -0,0 +1,54 @@
+/**
+ * The component to be rendered for the Download page but only
+ * when it is for media from YouTube.
+ */
+class YouTubeDownloader extends Main {
+    /**
+     * Constructing the application from React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.getRoute();
+        setTimeout(() => {
+            if (window.location.pathname != "/Search/") {
+                this.getMedia();
+            }
+        }, 1);
+    }
+
+    /**
+     * Rendering the component
+     * @returns {HTMLDivElement}
+     */
+    render() {
+        return (
+            <div class="YouTube">
+                <div>
+                    <video src={this.verifyFile()} controls autoplay></video>
+                </div>
+                <div>
+                    <button
+                        name="file_downloader"
+                        value={this.state.Media.YouTube.File.audio}
+                        onClick={this.getFile.bind(this)}
+                    >
+                        <i class="fa-solid fa-music"></i>
+                    </button>
+                </div>
+                <div>
+                    <button
+                        name="file_downloader"
+                        value={this.state.Media.YouTube.File.video}
+                        onClick={this.getFile.bind(this)}
+                    >
+                        <i class="fa-solid fa-video"></i>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}

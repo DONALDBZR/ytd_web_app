40d9f49d921234a7644f4cf96233865490288ea2
DONALDBZR <andygaspard@hotmail.com>
2025-01-04 11:41:54 +0400

    [UPDATE 2.12.61] The component to be rendered for the Download portal

---
+++ static/scripts/views/Main/Downloader.js
@@ -0,0 +1,30 @@
+/**
+ * The component to be rendered for the Download portal
+ */
+class Downloader extends React.Component {
+    /**
+     * Constructing the application from React's Component
+     * @param {*} props The properties of the component
+     */
+    constructor(props) {
+        super(props);
+    }
+
+    /**
+     * Rendering the component for the download page.
+     * @returns {React.Component}
+     */
+    render() {
+        return (
+            <main>
+                <div id="loading">
+                    <i class="fa-solid fa-spinner fa-spin"></i>
+                </div>
+                <Downloader />
+            </main>
+        );
+        if (window.location.pathname.includes("YouTube")) {
+            return <YouTubeDownloader />;
+        }
+    }
+}

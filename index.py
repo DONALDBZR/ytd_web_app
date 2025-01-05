883c52d8e5077f7fe4e8cb3e062b1d83e3a92e32
DONALDBZR <andygaspard@hotmail.com>
2025-01-05 23:53:51 +0400

    [UPDATE 2.12.95] Serving the JavaScript files.

--- index.py
+++ index.py
@@ -127,7 +127,7 @@ def serveScripts(file: str) -> Response:
     """
     response = send_from_directory('static/scripts/js', file)
     response.cache_control.max_age = 604800
-    response.cache_control.no_cache = False
+    response.cache_control.no_cache = False # type: ignore
     response.cache_control.public = True
     return response
 

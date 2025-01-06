40300106ee85549ccbe4c3692b800ae12c5fa8a0
DONALDBZR <andygaspard@hotmail.com>
2025-01-06 18:57:24 +0400

    [UPDATE 2.12.124] Sending the static file from the server.

--- Routes/Video.py
+++ Routes/Video.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response
+from Models.Video import Video
 
 
 Video_Portal: Blueprint = Blueprint("Video", __name__, "../Public/Video/")
@@ -7,14 +8,19 @@ The Routing for all the videos.
 """
 
 
-@Video_Portal.route("/<string:identifier>")
-def serveVideo(identifier: str) -> Response:
+@Video_Portal.route("/<string:name>", methods=['GET'])
+def serveVideo(name: str) -> Response:
     """
     Sending the static file from the server.
 
     Parameters:
-        identifier: string: Identifier of the video
+        name: string: The name of the video
 
-    Returns: Response
+    Returns:
+        Response
     """
-    return Video_Portal.send_static_file(identifier)
+    ok: int = 200
+    identifier: str = name.replace(".mp4", "")
+    video_management_system: Video = Video(identifier)
+    status: int = video_management_system.serveFile()
+    return Video_Portal.send_static_file(identifier) if status == ok else Response({}, status, mimetype="application/json")

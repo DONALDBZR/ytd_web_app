3f6ca24f72af91535738eeba0ee4f4a0fd95da9d
DONALDBZR <andygaspard@hotmail.com>
2025-02-03 20:12:39 +0400

    [UPDATE 3.3.21] Retrieving the loading time of a page.

--- static/scripts/js/tracker.js
+++ static/scripts/js/tracker.js
@@ -53,18 +53,17 @@ class Tracker {
             if (document.readyState === "complete") {
                 const loading_time = navigation_timing.loadEventEnd - navigation_timing.navigationStart;
                 resolve(loading_time);
-            } else {
-                window.addEventListener("load", () => {
-                    const navigation_entries_after_load = performance.getEntriesByType("navigation");
-                    if (navigation_entries_after_load.length > 0) {
-                        const navigation_timing_after_load = navigation_entries_after_load[0];
-                        const loading_time = navigation_timing_after_load.loadEventEnd - navigation_timing_after_load.navigationStart;
-                        resolve(loading_time);
-                    } else {
-                        resolve(null);
-                    }
-                });
             }
+            window.addEventListener("load", () => {
+                const navigation_entries_after_load = performance.getEntriesByType("navigation");
+                if (navigation_entries_after_load.length > 0) {
+                    const navigation_timing_after_load = navigation_entries_after_load[0];
+                    const loading_time = navigation_timing_after_load.loadEventEnd - navigation_timing_after_load.navigationStart;
+                    resolve(loading_time);
+                } else {
+                    resolve(null);
+                }
+            });
         });
     }
 

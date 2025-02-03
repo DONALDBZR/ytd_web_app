fbca22f666308afbcb64d9fc0ff85da5e0cf163a
DONALDBZR <andygaspard@hotmail.com>
2025-02-03 18:37:15 +0400

    [UPDATE 3.3.2] The tracker class which will track the user's activity on the application.

---
+++ static/scripts/js/tracker.js
@@ -0,0 +1,13 @@
+/**
+ * The tracker class which will track the user's activity on
+ * the application.
+ */
+class Tracker {
+    // /**
+    //  * Initializing the class.
+    //  */
+    // constructor() {
+    //     this.endpoint = "/track";
+    //     this.request_method = "POST";
+    // }
+}

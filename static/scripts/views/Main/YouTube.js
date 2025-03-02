97ed228193acef31c87e4e1fa5ae4ec1880173a8
DONALDBZR <andygaspard@hotmail.com>
2025-03-02 20:51:03 +0400

    [UPDATE 3.3.145] Handles the click event on a media card.

--- static/scripts/views/Main/YouTube.js
+++ static/scripts/views/Main/YouTube.js
@@ -208,6 +208,12 @@ class YouTube extends React.Component {
         })
         .then(() => {
             window.open(uniform_resource_locator, "_blank");
+        })
+        .catch((error) => {
+            console.error("An error occurred while sending the event or setting the route!\nError: ", error);
+            setTimeout(() => {
+                window.location.href = window.location.href;
+            }, delay);
         });
     };
 

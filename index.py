d537fb2bac6342265ec58e1a64f60f91c29d74d7
DONALDBZR <andygaspard@hotmail.com>
2025-03-03 13:25:11 +0400

    [UPDATE 3.4.11] Serving the stylesheets.

--- index.py
+++ index.py
@@ -178,6 +178,8 @@ def serveStylesheets(file: str) -> Response:
     response: Response
     if not validateFileName(file):
         return Response("Invalid File Name", 400)
+    if ".css" not in file:
+        return Response("Invalid File", 403)
     response = send_from_directory('static/stylesheets', file)
     response.cache_control.max_age = 604800
     response.cache_control.no_cache = False # type: ignore

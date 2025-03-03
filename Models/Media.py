d0b46e223cbedf9a3bf4b3908bfc7070cc925e95
DONALDBZR <andygaspard@hotmail.com>
2025-03-04 02:24:48 +0400

    [UPDATE 3.4.100] Retrieves the related content for a given channel from the YouTube database.  This function queries the database to find content associated with the provided channel name.  It returns a list of dictionaries with the content's identifier, duration, channel name, title, URL, and media identifier.

--- Models/Media.py
+++ Models/Media.py
@@ -396,32 +396,37 @@ class Media:
 
     def getRelatedChannelContents(self, channel: str) -> List[Dict[str, Union[str, int]]]:
         """
-        Retrieving the related content for the channel.
+        Retrieves the related content for a given channel from the
+        YouTube database.  This function queries the database to
+        find content associated with the provided channel name.  It
+        returns a list of dictionaries with the content's
+        identifier, duration, channel name, title, URL, and media
+        identifier.
 
         Parameters:
-            channel: string: The name of the channel.
+            channel (string): The name of the channel whose related content is being retrieved.
 
         Returns:
-            [{identifier: string, duration: string, channel: string, title: string, uniform_resource_locator: string, media_identifier: int}]
+            [{"identifier": string, "duration": string, "channel": string, "title": string, "uniform_resource_locator": string, "media_identifier": int}]
+
+        Raises:
+            Error: If there is an issue with querying the database, an error will be logged, and the function will return an empty list.
         """
         parameters: Tuple[str] = (channel,)
         response: List[Dict[str, Union[str, int]]] = []
-        database_response: Union[List[RowType], List[Dict[str, Union[str, int]]]] = self.getDatabaseHandler().getData(
-            parameters=parameters,
-            table_name="YouTube",
-            filter_condition="author = %s",
-            column_names="identifier, CONCAT(LPAD(FLOOR(length / 3600), 2, '0'), ':', LPAD(FLOOR(length / 60), 2, '0'), ':', LPAD(length % 60, 2, '0')) AS duration, author AS channel, title, CONCAT('https://www.youtube.com/watch?v=', identifier) AS uniform_resource_locator, Media AS media_identifier"
-        )
-        for index in range(0, len(database_response), 1):
-            response.append({
-                "identifier": str(database_response[index]["identifier"]), # type: ignore
-                "duration": str(database_response[index]["duration"]), # type: ignore
-                "channel": str(database_response[index]["channel"]), # type: ignore
-                "title": str(database_response[index]["title"]), # type: ignore
-                "uniform_resource_locator": str(database_response[index]["uniform_resource_locator"]), # type: ignore
-                "media_identifier": int(database_response[index]["media_identifier"]) # type: ignore
-            })
-        return response
+        try:
+            database_response: List[Dict[str, Union[str, int]]] = self.getDatabaseHandler().getData(
+                parameters=parameters,
+                table_name="YouTube",
+                filter_condition="author = %s",
+                column_names="identifier, CONCAT(LPAD(FLOOR(length / 3600), 2, '0'), ':', LPAD(FLOOR(length / 60), 2, '0'), ':', LPAD(length % 60, 2, '0')) AS duration, author AS channel, title, CONCAT('https://www.youtube.com/watch?v=', identifier) AS uniform_resource_locator, Media AS media_identifier"
+            ) # type: ignore
+            response = [{"identifier": str(channel_content["identifier"]), "duration": str(channel_content["duration"]), "channel": str(channel_content["channel"]), "title": str(channel_content["title"]), "uniform_resource_locator": str(channel_content["uniform_resource_locator"]), "media_identifier": int(channel_content["media_identifier"])} for channel_content in database_response]
+            self.getLogger().inform(f"The related channel contents have been successfully retrieved.\nStatus: 200\nChannel: {channel}\nAmount: {len(response)}")
+            return response
+        except Error as relational_database_server_error:
+            self.getLogger().error(f"There is an error between the model and the relational database server.\nError: {relational_database_server_error}")
+            return response
 
     def _getPayload(self, identifier: str) -> Dict[str, str]:
         """

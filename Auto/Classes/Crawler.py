3e45687a67579b5dcf3efdeecc33dfd208ed16a0
DONALDBZR <andygaspard@hotmail.com>
2025-03-05 01:03:46 +0400

    [UPDATE 3.4.180] Reading the `robots.txt` file for the specified Uniform Resource Locator and updating the internal robot parsers.  This method attempts to read the `robots.txt` file using the provided parser.  If an error occurs during the reading process, it logs the error and removes the corresponding entry from the internal robot parsers dictionary.  The result is stored in the internal robot parsers dictionary with the uniform resource locator as the key.

--- Auto/Classes/Crawler.py
+++ Auto/Classes/Crawler.py
@@ -716,7 +716,7 @@ class Crawler:
         except Exception as error:
             self.getLogger().error(f"An error occured while reading the robots.txt file.\nError: {error}\nUniform Resource Locator: {uniform_resource_locator}")
             if uniform_resource_locator in self.getRobotParsers():
-                del self.getRobotParsers()[uniform_resource_locator]None
+                del self.getRobotParsers()[uniform_resource_locator]
 
     def retrieveData(self, referrer: str, index: int = 0) -> None:
         """

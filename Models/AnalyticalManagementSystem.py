e8a5fe30a5b0732789eedf0c02c5ea9bf5303c17
DONALDBZR <andygaspard@hotmail.com>
2025-03-08 20:25:00 +0400

    [UPDATE 3.4.322] Inserting a new event click record into the `"Events"` table of the database.

--- Models/AnalyticalManagementSystem.py
+++ Models/AnalyticalManagementSystem.py
@@ -723,30 +723,31 @@ class AnalyticalManagementSystem:
 
     def postEventClick(self, device: int, event_type: int, network_location: int, click: int) -> int:
         """
-        Adding the event that is specifically click.
+        Inserting a new event click record into the `"Events"` table of the database.
+
+        This method:
+        - Inserts data related to the click event into the `"Events"` table.
+        - Logs success or failure messages.
+        - Returns a status code representing the result of the operation.
 
         Parameters:
-            device: int: The identifier of the device.
-            event_type: int: The identifier of the event type.
-            network_location: int: The identifier of the network location.
-            click: int: The identifier of the click.
+            device (int): The device identifier where the event occurred.
+            event_type (int): The type of the event.
+            network_location (int): The network location identifier.
+            click (int): The identifier for the event.
 
         Returns:
             int
         """
         parameters: Tuple[str, Union[str, None], int, int, int, int, int] = (self.getUniformResourceLocator(), self.getReferrer(), self.getTimestamp(), device, event_type, network_location, click)
-        try:
-            self.getDatabaseHandler().postData(
-                table="Events",
-                columns="uniform_resource_locator, referrer, timestamp, Device, EventType, NetworkLocation, Click",
-                values="%s, %s, %s, %s, %s, %s, %s",
-                parameters=parameters # type: ignore
-            )
-            self.getLogger().inform(f"The data has been successfully inserted in the Event table.\nStatus: {self.created}")
-            return self.created
-        except DatabaseHandlerError as error:
-            self.getLogger().error(f"An error occurred while inserting data in the Event table.\nError: {error}")
-            return self.service_unavailable
+        response: bool = self.getDatabaseHandler().postData(
+            table="Events",
+            columns="uniform_resource_locator, referrer, timestamp, Device, EventType, NetworkLocation, Click",
+            values="%s, %s, %s, %s, %s, %s, %s",
+            parameters=parameters # type: ignore
+        )
+        self.getLogger().inform(f"The data has been successfully inserted in the Event table.\nStatus: {self.created}") if response else self.getLogger().error(f"An error occurred while inserting data in the Event table.\nStatus: {self.service_unavailable}")
+        return self.created if response else self.service_unavailable
 
     def postEventSearchSubmitted(self, device: int, event_type: int, network_location: int, search_submitted: int) -> int:
         """

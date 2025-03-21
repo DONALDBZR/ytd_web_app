3ea7db6c9c4a8f7f9d27cfb689750524bf152bde
DONALDBZR <andygaspard@hotmail.com>
2025-03-21 21:34:24 +0400

    [UPDATE 3.4.394] The component that will be used for the Search page that will have the data for the Media.

--- static/scripts/views/Main/Media.js
+++ static/scripts/views/Main/Media.js
@@ -1,11 +1,9 @@
 /**
- * The component that will be used for the Search page that
- * will have the data for the Media.
+ * The component that will be used for the Search page that will have the data for the Media.
  */
 class Media extends React.Component {
     /**
-     * Constructing the component that will be used for the Media
-     * data type.
+     * Constructing the component that will be used for the Media data type.
      * @param {*} props
      */
     constructor(props) {
@@ -14,7 +12,7 @@ class Media extends React.Component {
 
     /**
      * Rendering the component
-     * @returns {React.Component}
+     * @returns {React.JSX.Component}
      */
     render() {
         return (

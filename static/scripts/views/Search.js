a37b5947704de7ce3a0753bdeff7d992219c2df8
DONALDBZR <andygaspard@hotmail.com>
2025-03-21 19:42:43 +0400

    [UPDATE 3.4.377] The component for the search pages.

--- static/scripts/views/Search.js
+++ static/scripts/views/Search.js
@@ -8,15 +8,37 @@ class Search extends React.Component {
      */
     constructor(props) {
         super(props);
+        /**
+         * The flag for checking that the page is embedded or not.
+         * @type {boolean}
+         */
+        this.embedded = false;
     }
-    
+
+    /**
+     * Mounting the component.
+     * @returns {void}
+     */
+    componentDidMount() {
+        this.checkEmbedded();
+    }
+
+    /**
+     * Checking if the page is embedded or not.
+     * @returns {void}
+     */
+    checkEmbedded() {
+        if (window.top !== window.self) {
+            this.embedded = true;
+        }
+    }
 
     /**
      * Rendering the component.
-     * @returns {React.Component}
+     * @returns {React.JSX.Element[]|null}
      */
     render() {
-        return [<Header />, <Main />, <Footer />];
+        return (this.embedded) ? null : [<Header />, <Main />, <Footer />];
     }
 }
 

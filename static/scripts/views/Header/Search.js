9b1415829b6f673508f1484772a721ae56cb97d5
DONALDBZR <andygaspard@hotmail.com>
2025-03-21 21:20:32 +0400

    [UPDATE 3.4.391] Retrieving the response of the Media API for the search data.

--- static/scripts/views/Header/Search.js
+++ static/scripts/views/Header/Search.js
@@ -302,8 +302,7 @@ class HeaderSearch extends React.Component {
     }
 
     /**
-     * Retrieving the response of the Media API for the search
-     * data.
+     * Retrieving the response of the Media API for the search data.
      * @param {string} platform The platform to be searched on.
      * @param {string} search The search data to be searched.
      * @returns {Promise<{status: number, data: {uniform_resource_locator: string, author: string, title: string, identifier: string, author_channel: string, views: number, published_at: string | null, thumbnail: string, duration: string, audio_file: string | null, video_file: string | null}}>}
@@ -313,6 +312,13 @@ class HeaderSearch extends React.Component {
             method: "GET",
         });
         const data = await response.json();
+        if (!data.data || typeof data.data !== "object") {
+            console.error("Invalid data received from the server.");
+            return {
+                status: 400,
+                data: {},
+            };
+        }
         return {
             status: response.status,
             data: data.data,

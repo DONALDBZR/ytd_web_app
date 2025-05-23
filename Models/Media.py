e73dbb338565820d3f8648f6ba26f2f630a87419
DONALDBZR <andygaspard@hotmail.com>
2025-05-24 00:54:21 +0400

    [UPDATE 3.7.15] Extracting the YouTube video identifier from the search URL.

--- Models/Media.py
+++ Models/Media.py
@@ -305,8 +305,8 @@ class Media:
         identifier_regex: str = r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})"
         match_identifier: Union[Match[str], None] = match(identifier_regex, self.getSearch())
         if not match_identifier:
-            self.getLogger().error("The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
-            raise ValueError("The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
+            self.getLogger().error(f"The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
+            raise ValueError(f"The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
         return match_identifier.group(1) # type: ignore
 
     def getRelatedContents(self, identifier: str) -> Dict[str, Union[int, List[Dict[str, str]]]]:

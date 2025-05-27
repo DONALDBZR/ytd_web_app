5a9f63df115b18488fa060f2f87f273099a45202
DONALDBZR <andygaspard@hotmail.com>
2025-05-27 18:03:24 +0400

    [UPDATE 3.7.19] Extracting the YouTube video identifier from the search URL.

--- Models/Media.py
+++ Models/Media.py
@@ -280,8 +280,8 @@ class Media:
         response: Dict[str, Union[int, Dict[str, Union[str, int, None]]]]
         self._YouTubeDownloader: YouTube_Downloader = YouTube_Downloader(self.getSearch(), self.getIdentifier())
         identifier: str = self._getIdentifier()
-        print(f"Function: Models.Media.handleYouTube\nIdentifier: {identifier}")
         filename: str = f"{self.getDirectory()}/{identifier}.json"
+        self.getLogger().debug(f"Function: Models.Media.handleYouTube\nIdentifier: {identifier}\nFile Name: {filename}")
         status: int = 200 if self.getReferer() is None else 201
         youtube: Dict[str, Union[str, int, None]] = self._YouTubeDownloader.search() if self.getReferer() is None else self._YouTubeDownloader.retrievingStreams() # type: ignore
         media: Dict[str, Dict[str, Dict[str, Union[str, int, None]]]] = {
@@ -318,7 +318,7 @@ class Media:
         if not match_identifier:
             self.getLogger().error(f"The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
             raise ValueError(f"The uniform resource locator is not supported.\nStatus: 400\nSearch: {self.getSearch()}")
-        return f"/shorts/{match_identifier.group(1)}" if "/shorts/" in self.getSearch() else match_identifier.group(1)
+        return f"shorts/{match_identifier.group(1)}" if "/shorts/" in self.getSearch() else match_identifier.group(1)
 
     def getRelatedContents(self, identifier: str) -> Dict[str, Union[int, List[Dict[str, str]]]]:
         """

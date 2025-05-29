540a5583cea7b7222c463187a1eac8fc489ba47a
DONALDBZR <andygaspard@hotmail.com>
2025-05-29 22:48:45 +0400

    [UPDATE 3.7.47] Retrieving the host name which will be used as the platform for the search from the parsed uniform resource locator.

--- static/scripts/components/Header/Search.js
+++ static/scripts/components/Header/Search.js
@@ -112,6 +112,22 @@ class HeaderSearch extends Component {
     }
 
     /**
+     * Retrieving the host name which will be used as the platform for the search from the parsed uniform resource locator.
+     * 
+     * Currently, this function only supports YouTube uniform resource locators.  If the hostname does not match a known YouTube format, it throws an error.
+     * @param {URL} uniform_resource_locator The parsed uniform resource locator.
+     * @returns {string} The name of the supported platform.
+     * @throws {Error} If the URL does not belong to a supported platform.
+     */
+    getPlatform(uniform_resource_locator) {
+        const hostname = uniform_resource_locator.hostname.toLowerCase();
+        if (hostname == "youtu.be" || hostname.includes("youtube")) {
+            return "youtube";
+        }
+        throw new Error("The platform is not supported by the application.");
+    }
+
+    /**
      * Handling the form submission event to extract metadata from a media URL.
      * 
      * This function prevents the default form submission behavior, displays a loading icon, parses the user-provided media URL to determine the platform, media type (video or shorts), and identifier, and then initiates metadata fetching via `searchMediaMetadata`.

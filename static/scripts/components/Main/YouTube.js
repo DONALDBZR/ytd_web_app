ca2ef05a59080899c19201a63b1f9a63f22bf8f7
DONALDBZR <andygaspard@hotmail.com>
2025-06-05 13:51:32 +0400

    [UPDATE 3.7.68] Retrieving the host name which will be used as the platform for the search from the parsed uniform resource locator.

--- static/scripts/components/Main/YouTube.js
+++ static/scripts/components/Main/YouTube.js
@@ -91,6 +91,22 @@ class YouTube extends Component {
     }
 
     /**
+     * Retrieving the host name which will be used as the platform for the search from the parsed uniform resource locator.
+     * 
+     * Currently, this function only supports YouTube uniform resource locators.  If the hostname does not match a known YouTube format, it throws an error.
+     * @param {URL} uniform_resource_locator The parsed uniform resource locator.
+     * @returns {string} The name of the supported platform.
+     * @throws {Error} If the URL does not belong to a supported platform.
+     */
+    getPlatform(uniform_resource_locator) {
+        const hostname = uniform_resource_locator.hostname.toLowerCase();
+        if (hostname == "youtu.be" || hostname.includes("youtube")) {
+            return "youtube";
+        }
+        throw new Error("The platform is not supported by the application.");
+    }
+
+    /**
      * Handling the media retrieval process from a given YouTube uniform resource locator.
      * 
      * This method prevents the default form or link behavior, displays a loading indicator, parses the YouTube uniform resource locator, determines the media type, extracts the unique identifier, and initiates the media download process.  If an error occurs during uniform resource locator processing, it logs the error to the console.

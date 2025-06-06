f40be58fed54d476c2a5b365c470d4a9449d27a3
DONALDBZR <andygaspard@hotmail.com>
2025-06-07 02:45:23 +0400

    [UPDATE 3.7.81] Rendering the template needed which will import the web-worker.

--- Routes/Download.py
+++ Routes/Download.py
@@ -9,6 +9,8 @@ from flask import Blueprint, Response, render_template, request, send_file
 from Models.SecurityManagementSystem import Security_Management_System, Union, Environment
 from typing import Dict
 
+from index import isEmbeddedRequest
+
 
 Download_Portal: Blueprint = Blueprint("Download", __name__)
 """
@@ -40,18 +42,48 @@ def downloadPage(identifier: str) -> Response:
     web-worker.
 
     Parameters:
-        identifier: string: Identifier of the media content to be searched.
+        identifier (str): Identifier of the media content to be searched.
 
-    Returns: Response
+    Returns:
+        Response
     """
-    template: str = render_template(
-        'Download.html',
-        google_analytics_key=ENV.getGoogleAnalyticsKey()
-    )
+    is_embedded: bool = isEmbeddedRequest(request)
+    status: int = 403 if is_embedded else 200
     mime_type: str = "text/html"
-    status: int = 200
-    return Response(template, status, mimetype=mime_type)
-
+    if is_embedded:
+        return Response(
+            response="Forbidden",
+            status=status,
+            mimetype=mime_type
+        )
+    nonce: str = SecurityManagementSystem.getNonce()
+    template: str = render_template(
+        template_name_or_list='Download.html',
+        nonce=nonce,
+    )
+    content_security_policy: str = "; ".join([
+        "default-src 'self'",
+        f"script-src 'self' 'nonce-{nonce}' https://cdnjs.cloudflare.com",
+        "style-src 'self' https://fonts.cdnfonts.com https://cdnjs.cloudflare.com",
+        "img-src 'self' data: https://i.ytimg.com",
+        "font-src 'self' https://fonts.cdnfonts.com https://cdnjs.cloudflare.com",
+        "connect-src 'self'",
+        "frame-src 'none'",
+        "object-src 'none'",
+        "base-uri 'self'",
+        "form-action 'self'"
+    ])
+    response: Response = Response(
+        response=template,
+        status=status,
+        mimetype=mime_type
+    )
+    response.cache_control.max_age = 604800
+    response.cache_control.no_cache = False  # type: ignore
+    response.cache_control.public = True
+    response.content_security_policy = content_security_policy
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
 
 @Download_Portal.route('/', methods=['POST'])
 def downloadFile() -> Response:

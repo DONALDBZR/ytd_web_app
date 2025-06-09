9b3ec6c3cc4947a477795f54aafeb9e46ff2679c
DONALDBZR <andygaspard@hotmail.com>
2025-06-09 21:23:37 +0400

    [UPDATE 3.8.15] Sending a PUT request to the server to update the user's session with the selected color scheme.  Validating the provided scheme before making the request.

--- static/scripts/components/Header/Search.js
+++ static/scripts/components/Header/Search.js
@@ -554,31 +554,29 @@ class HeaderSearch extends Component {
     }
 
     /**
-     * Checking the response of the server to handle it correctly.
-     * @param {string} color_scheme The color scheme of the application.
+     * Sending a PUT request to the server to update the user's session with the selected color scheme.  Validating the provided scheme before making the request.
+     * @param {string} color_scheme - The desired color scheme to set.
      * @returns {Promise<number>}
      */
     async updateSession(color_scheme) {
-        const allowed_color_schemes = ["light", "dark"];
-        if (!allowed_color_schemes.includes(color_scheme)) {
-            console.error(`The color scheme is invalid.\nStatus: 400\nColor Scheme: ${color_scheme}`);
-            return 400;
-        }
-        const response = await fetch("/Session/", {
-            method: "PUT",
-            body: JSON.stringify({
-                Client: {
-                    color_scheme: color_scheme,
+        try {
+            await this.isAllowedColorScheme(color_scheme);
+            const response = await fetch("/Session/", {
+                method: "PUT",
+                body: JSON.stringify({
+                    Client: {
+                        color_scheme: color_scheme,
+                    },
+                }),
+                headers: {
+                    "Content-Type": "application/json",
                 },
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (response.status == 202) {
-            localStorage.removeItem("session");
+            });
+            return response.status;
+        } catch (error) {
+            console.error(`The application has failed to update the session.\nError: ${error.message}`);
+            throw new Error(error.message);
         }
-        return response.status;
     }
 
     /**

976fd5069d9970ace6ff336f2805a5b9a7ac8c6f
DONALDBZR <andygaspard@hotmail.com>
2025-06-10 19:39:55 +0400

    [UPDATE 3.8.69] Retrieving the Trend data from the `localStorage`.

--- static/scripts/components/utilities/Main.js
+++ static/scripts/components/utilities/Main.js
@@ -1,6 +1,19 @@
 /**
  * The utility class of the Main component.
  */
-class Main {}
+class Main {
+    /**
+     * Retrieving the Trend data from the `localStorage`.
+     * @returns {{trend: ?[{audio_file: ?string, author: string, author_channel: string, duration: string, identifier: string, published_at: string, thumbnail: string, title: string, uniform_resource_locator: string, video_file: ?string, views: number}], data_loaded: boolean}}
+     */
+    getTrends() {
+        const trend = (localStorage.getItem("trend") != null) ? JSON.parse(localStorage.getItem("trend")).data : null;
+        const data_loaded = (trend != null && window.Tracker);
+        return {
+            trend: trend,
+            data_loaded: data_loaded,
+        };
+    }
+}
 
 export default Main;

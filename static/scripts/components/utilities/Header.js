456c5e29aa46310659f1aaaec0fa4b04cacbae3e
DONALDBZR <andygaspard@hotmail.com>
2025-06-10 20:48:51 +0400

    [UPDATE 3.8.85] The utility class of the Header component.

--- static/scripts/components/utilities/Header.js
+++ static/scripts/components/utilities/Header.js
@@ -1,7 +1,10 @@
+import Application from "./Application";
+
+
 /**
  * The utility class of the Header component.
  */
-class Header {
+class Header extends Application {
     /**
      * * Sanitizing a string by escaping special HTML characters.
      * 
@@ -75,22 +78,6 @@ class Header {
             return uniform_resource_locator.pathname.slice(1);
         }
         return uniform_resource_locator.searchParams.get("v");
-    }
-
-    /**
-     * Retrieving the host name which will be used as the platform for the search from the parsed uniform resource locator.
-     * 
-     * Currently, this function only supports YouTube uniform resource locators.  If the hostname does not match a known YouTube format, it throws an error.
-     * @param {URL} uniform_resource_locator The parsed uniform resource locator.
-     * @returns {string} The name of the supported platform.
-     * @throws {Error} If the URL does not belong to a supported platform.
-     */
-    getPlatform(uniform_resource_locator) {
-        const hostname = uniform_resource_locator.hostname.toLowerCase();
-        if (hostname == "youtu.be" || hostname.includes("youtube")) {
-            return "youtube";
-        }
-        throw new Error("The platform is not supported by the application.");
     }
 
     /**

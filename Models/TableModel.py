e450d39c6faff88637b22442f0b1c845d937a0e9
DONALDBZR <andygaspard@hotmail.com>
2025-07-21 00:50:53 +0400

    [UPDATE 4.2.24] Fetching the column names and types for the table from the database.

--- Models/TableModel.py
+++ Models/TableModel.py
@@ -1,4 +1,5 @@
-from Models.DatabaseHandler import Database_Handler, List
+from Models.DatabaseHandler import Database_Handler, List, RowType, Tuple, Any
+from typing import Dict
 
 
 class TableModel:
@@ -10,9 +11,13 @@ class TableModel:
     """
     The database handler instance to interact with the database.
     """
-    __fields: List[str]
+    __fields: Tuple[List[str], Dict[str, str]]
     """
     A list of fields in the table.
+    """
+    __mysql_field_types: Dict[str, Any]
+    """
+    A dictionary mapping field names to their MySQL types.
     """
 
     def __init__(
@@ -29,6 +34,30 @@ class TableModel:
         """
         self.setDatabaseHandler(database_handler)
         self.setFields(self._getFields())
+        self.setMySqlFieldTypes({
+            "int": int,
+            "bigint": int,
+            "smallint": int,
+            "tinyint": int,
+            "mediumint": int,
+            "float": float,
+            "double": float,
+            "decimal": float,
+            "char": str,
+            "varchar": str,
+            "text": str,
+            "mediumtext": str,
+            "longtext": str,
+            "date": str,
+            "datetime": str,
+            "timestamp": str,
+            "time": str,
+            "enum": str,
+            "set": str,
+            "blob": bytes,
+            "binary": bytes,
+            "varbinary": bytes
+        })
         self.setModelAttributes(kwargs)
 
     def getTableName(self) -> str:
@@ -43,8 +72,26 @@ class TableModel:
     def setDatabaseHandler(self, database_handler: Database_Handler) -> None:
         self.__database_handler = database_handler
 
-    def getFields(self) -> List[str]:
+    def getFields(self) -> Tuple[List[str], Dict[str, str]]:
         return self.__fields
 
-    def setFields(self, fields: List[str]) -> None:
+    def setFields(self, fields: Tuple[List[str], Dict[str, str]]) -> None:
         self.__fields = fields
+
+    def getMySqlFieldTypes(self) -> Dict[str, Any]:
+        return self.__mysql_field_types
+
+    def setMySqlFieldTypes(self, mysql_field_types: Dict[str, Any]) -> None:
+        self.__mysql_field_types = mysql_field_types
+
+    def _getFields(self) -> Tuple[List[str], Dict[str, str]]:
+        """
+        Fetching the column names and types for the table from the database.
+
+        Returns:
+            Tuple[List[str], Dict[str, str]]: A tuple containing a list of field names and a dictionary mapping field names to their MySQL types.
+        """
+        database_response: List[RowType] = self.getDatabaseHandler().getData(f"SHOW COLUMNS FROM {self.getTableName()}")
+        fields: List[str] = [str(column["Field"]) for column in database_response] # type: ignore
+        field_types: Dict[str, str] = {str(column["Field"]): str(column["Type"]) for column in database_response} # type: ignore
+        return fields, field_types

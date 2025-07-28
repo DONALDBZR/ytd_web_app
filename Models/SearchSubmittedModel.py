17ab6ad42ef9f77b80b7d6fe60dea237ff6e646b
DONALDBZR <andygaspard@hotmail.com>
2025-07-28 23:20:09 +0400

    [UPDATE 4.2.75] Retrieving all rows from the `Search_Submitted` table that match the provided search term.

--- Models/SearchSubmittedModel.py
+++ Models/SearchSubmittedModel.py
@@ -1,4 +1,4 @@
-from Models.TableModel import Table_Model, Database_Handler, List, RowType
+from Models.TableModel import Table_Model, Database_Handler, List, RowType, Tuple
 
 
 class Search_Submitted(Table_Model):
@@ -22,3 +22,23 @@ class Search_Submitted(Table_Model):
             table_name="Search_Submitted",
             **kwargs
         )
+
+    @classmethod
+    def getBySearchTerm(cls, database_handler: Database_Handler, search_term: str) -> List["Search_Submitted"]:
+        """
+        Retrieving all rows from the `Search_Submitted` table that match the provided search term.
+
+        Parameters:
+            database_handler (Database_Handler): The database handler to execute queries.
+            search_term (str): The search term to filter records by.
+
+        Returns:
+            List[Search_Submitted]: A list of matching Search_Submitted instances. Returns an empty list if no matches are found.
+        """
+        temporary_instance: "Search_Submitted" = cls(database_handler)
+        query: str = f"SELECT * FROM {temporary_instance.getTableName()} WHERE search_term = %s"
+        parameters: Tuple[str] = (search_term,)
+        response: List[RowType] = temporary_instance.getDatabaseHandler().getData(query, parameters)
+        if not response:
+            return []
+        return [cls(database_handler, **row) for row in response] # type: ignore

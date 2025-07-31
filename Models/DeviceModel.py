c168e5707ed9f0adaf39a9b1552514813f9bfed9
DONALDBZR <andygaspard@hotmail.com>
2025-08-01 01:11:07 +0400

    [UPDATE 4.2.100] Fetching device records from the database that match the given user agent and screen resolution.

--- Models/DeviceModel.py
+++ Models/DeviceModel.py
@@ -22,3 +22,29 @@ class Device(Table_Model):
             table_name="Devices",
             **kwargs
         )
+
+    @classmethod
+    def getByDeviceType(
+        cls,
+        database_handler: Database_Handler,
+        user_agent: str,
+        screen_resolution: str
+    ) -> List["Device"]:
+        """
+        Fetching device records from the database that match the given user agent and screen resolution.
+
+        Args:
+            database_handler (Database_Handler): The database handler to use for the query.
+            user_agent (str): The user agent string to match.
+            screen_resolution (str): The screen resolution to match.
+
+        Returns:
+            List[Device]: A list of Device instances that match the given criteria.
+        """
+        temporary_instance: "Device" = cls(database_handler)
+        query: str = f"SELECT * FROM {temporary_instance.getTableName()} WHERE user_agent = %s AND screen_resolution = %s"
+        parameters: Tuple[str, str] = (user_agent, screen_resolution)
+        response: List[RowType] = temporary_instance.getDatabaseHandler().getData(query, parameters)
+        if not response:
+            return []
+        return [cls(database_handler, **row) for row in response] # type: ignore

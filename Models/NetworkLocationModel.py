4ef3023177ba9728e2f5c27ad7ae8d32f3e7bcc7
DONALDBZR <andygaspard@hotmail.com>
2025-08-01 00:34:21 +0400

    [UPDATE 4.2.89] Retrieving all network location records that match a given IP address and geographical location.

--- Models/NetworkLocationModel.py
+++ Models/NetworkLocationModel.py
@@ -22,3 +22,29 @@ class Network_Location(Table_Model):
             table_name="NetworkLocation",
             **kwargs
         )
+
+    @classmethod
+    def getByIpAndLocation(
+        cls,
+        database_handler: Database_Handler,
+        ip_address: str,
+        location: List[float]
+    ) -> List["Network_Location"]:
+        """
+        Retrieving all network location records that match a given IP address and geographical location.
+
+        Args:
+            database_handler (Database_Handler): Instance for interacting with the database.
+            ip_address (str): The IP address to query.
+            location (List[float]): A list containing the latitude and longitude [latitude, longitude].
+
+        Returns:
+            List[Network_Location]: A list of Network_Location instances matching the given criteria.
+        """
+        temporary_instance: "Network_Location" = cls(database_handler)
+        query: str = f"SELECT * FROM {temporary_instance.getTableName()} WHERE ip_address = %s AND latitude = %s AND longitude = %s"
+        parameters: Tuple[str, float, float] = (ip_address, location[0], location[1])
+        response: List[RowType] = temporary_instance.getDatabaseHandler().getData(query, parameters)
+        if not response:
+            return []
+        return [cls(database_handler, **row) for row in response] # type: ignore

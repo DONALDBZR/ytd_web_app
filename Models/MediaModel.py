a8dbcd6863ee080f3421a69e77d32708c4ae14ea
DONALDBZR <andygaspard@hotmail.com>
2025-08-03 18:04:49 +0400

    [UPDATE 4.2.106] It inherits from `Table_Model` and is used to interact with a `Media` table in a database.

---
+++ Models/MediaModel.py
@@ -0,0 +1,31 @@
+from Models.TableModel import Table_Model, Database_Handler
+
+
+class Media(Table_Model):
+    """
+    It inherits from `Table_Model` and is used to interact with a `Media` table in a database.
+    """
+    def __init__(
+        self,
+        database_handler: Database_Handler,
+        **kwargs
+    ):
+        """
+        Initializing a Media model instance with the specified database handler.
+
+        Parameters:
+            database_handler (Database_Handler): The handler for interacting with the database.
+            **kwargs: Additional keyword arguments passed to the parent class.
+
+        Inherits:
+            Table_Model: Initializes the base model with the table name set to "Media".
+        """
+        super().__init__(
+            database_handler,
+            table_name="Media",
+            **kwargs
+        )
+
+    def create(self) -> bool:
+        query: str = f"CREATE TABLE IF NOT EXISTS `{self.getTableName()}` (identifier INT PRIMARY KEY AUTO_INCREMENT, `value` VARCHAR(8))"
+        return self.getDatabaseHandler().createTable(query, None)
